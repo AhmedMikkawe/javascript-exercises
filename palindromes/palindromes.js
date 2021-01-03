6849d58897dc891a5229e59e10609a920c3040bd
Ahmed Mikkawe <dev.ahmedmikkawe@gmail.com>
2021-01-03 17:16:00 +0200

    completed palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    let newString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    let array = newString.split('');
+    let arrayReversed = array.reverse().join('');
+    if (arrayReversed == newString) {
+        return true;
+    } else{
+        return false;
+    }
+    
+};
 
-}
-
-module.exports = palindromes
+module.exports = palindromes;
