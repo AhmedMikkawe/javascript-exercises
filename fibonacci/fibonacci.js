c295ec21669299788ffa9f20baec3370d80d027c
Ahmed Mikkawe <dev.ahmedmikkawe@gmail.com>
2021-01-04 14:23:21 +0200

    completed fibonacci exercise

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(location) {
+    if (location < 0) {
+        return 'OOPS';
+    }
+    if(location == 0){
+        return 0;
+    }
+    let f0 = 0;
+    let f1 = 1;
+    for (let i = 1; i < location; i++) {
+        let fN = f1;
+        f1 = f0 + f1;
+        f0 = fN;
+    }
+    return f1;
 }
 
 module.exports = fibonacci
